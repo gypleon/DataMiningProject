2ec43f52c7a2837a901ba0824d1855d4edd78bd7
gypleon <gyp_leon@hotmail.com>
2016-12-01 22:42:08 +0800

    mlp

--- classifier/mlp.py
+++ classifier/mlp.py
@@ -2,7 +2,12 @@
 
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing as skpp
+
+from sklearn.neural_network import MLPClassifier
 
 PATH_DATASET="../preprocessed/encoded_python_features.csv"
 PATH_LABELS="../preprocessed/encoded_python_labels.csv"
@@ -16,11 +21,19 @@ def preprocessing(dataset):
     enc.fit(dataset)
     return enc.transform(dataset).toarray()
 
-
 def main():
     dataset = load_dataset(PATH_DATASET)
-    array = preprocessing(dataset)
-    print array
+    labels = load_dataset(PATH_DATASET)
+    dataset = preprocessing(dataset)
+
+    train_data = dataset[:18000]
+    train_labels = labels[:18000]
+    test_data = dataset[18001:]
+    test_labels = labels[18001:]
+
+    mlp = MLPClassifier()
+    mlp.fit(train_data, train_labels)
+    print mlp.score(test_data, test_labesl)
 
 
 if __name__ == "__main__":
