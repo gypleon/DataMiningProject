79349de287065b2fb3520117ae46782a590dff71
LeonGuo <leonguo@localhost.localdomain>
2016-12-06 10:06:57 +0800

    update

--- classifier/diabreadm_classifiers.py
+++ classifier/diabreadm_classifiers.py
@@ -2,8 +2,8 @@
 
 import numpy as np
 
-# import matplotlib.pyplot as plt
-# from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 from sklearn.model_selection import train_test_split
 
@@ -12,7 +12,12 @@ from sklearn import preprocessing as skpp
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 from sklearn.naive_bayes import MultinomialNB
+
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
 
 PATH_DATASET="../preprocessed/encoded_python_features.csv"
 PATH_LABELS="../preprocessed/encoded_python_labels.csv"
@@ -36,21 +41,29 @@ def main():
 
     train_data, test_data, train_labels, test_labels = train_test_split(dataset, labels, test_size=.2, random_state=66)
 
-    mlp = MLPClassifier(hidden_layer_sizes=(16, 16), max_iter=300, verbose=True)
-    mlp.fit(train_data, train_labels)
-    print "MLP", mlp.score(test_data, test_labels)
+    dt = DecisionTreeClassifier()
+    dt.fit(train_data, train_labels)
+    print "Tree", dt.score(test_data, test_labels)
+    # cross_val_score(dt, train_data, train_labels, cv=10)
+    # tree.export_graphviz(dt, out_file="tree.dot", max_depth=5, class_names=["Non-Readmission", "Readmission"])
+    print metrics.confusion_matrix(test_labels, dt.predict(test_data), labels=[0, 1])
+    exit()
 
-    rfc = RandomForestClassifier(criterion="entropy", max_features=None, n_jobs=-1, verbose=True)
+    mlp = MLPClassifier(hidden_layer_sizes=(16, 16), max_iter=300, verbose=False)
+    mlp.fit(train_data, train_labels)
+    print "Multi Layer Perceptron", mlp.score(test_data, test_labels)
+
+    rfc = RandomForestClassifier(criterion="entropy", max_features=None, n_jobs=-1, verbose=False)
     rfc.fit(train_data, train_labels)
-    print "RFC", rfc.score(test_data, test_labels)
+    print "Random Forest", rfc.score(test_data, test_labels)
 
     abc = AdaBoostClassifier()
     abc.fit(train_data, train_labels)
-    print "ABC", abc.score(test_data, test_labels)
+    print "AdaBoost", abc.score(test_data, test_labels)
 
     mnb = MultinomialNB()
     mnb.fit(train_data, train_labels)
-    print "MNB", mnb.score(test_data, test_labels)
+    print "Nultinomial Naive Bayes", mnb.score(test_data, test_labels)
 
 
 
